6d25ba2c277c5fc20a1ddb347ad9c661fc9b21eb
blade365z <amitabhyo@gmail.com>
2021-04-04 15:07:13 +0530

    logic modified

--- index.js
+++ index.js
@@ -16,15 +16,22 @@ console.log(graph)
 //weights - HashMap; Keeping track of the weights 
 //parentMap -Hashmap; Keeping track of the parents
 
-var visited = [];
+var unvisited = [];
 var weights = {};
 var parentMap = {};
-
-
-
 
 //Functions to be implmeneted
 //calulateShortest()
 //Dijkstras()
 
+const calulateShortest = () => {
 
+}
+
+const dijksta = (graph) => {
+  Object.keys(graph).map(node=>unvisited.push(node));
+  while(unvisited.length!==0){
+    
+  }
+}
+dijksta(graph);
